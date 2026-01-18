74dd1675a2175413f98855617619371f49a01282
harris8099 <harris1238099@gmail.com>
2026-01-18 19:19:32 +0530

    18-01-2026_19:19

--- MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+++ MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -84,6 +84,7 @@
 
 #define EXTI_BASEADDR						(APB2PERIPH_BASEADDR + 0x3C00)
 #define SPI1_BASEADDR 						(APB2PERIPH_BASEADDR + 0x3000)
+#define SPI4_BASEADDR						(APB2PERIPH_BASEADDR + 0x3400)
 #define SYSCFG_BASEADDR 					(APB2PERIPH_BASEADDR + 0x3800)
 #define USART1_BASEADDR						(APB2PERIPH_BASEADDR + 0x1000)
 #define USART6_BASEADDR						(APB2PERIPH_BASEADDR + 0x1400)
@@ -155,6 +156,21 @@ typedef struct{
 }EXTI_RegDef_t;
 
 /*
+ * peripheral register definition structure for SPI
+ */
+typedef struct{
+	__vo uint32_t CR1;
+	__vo uint32_t CR2;
+	__vo uint32_t SR;
+	__vo uint32_t DR;
+	__vo uint32_t CRCPR;
+	__vo uint32_t RXCRCR;
+	__vo uint32_t TXCRCR;
+	__vo uint32_t I2SCFGR;
+	__vo uint32_t I2SPR;
+}SPI_RegDef_t;
+
+/*
  * peripheral definition structure for SYSCFG
  */
 typedef struct{
@@ -182,6 +198,11 @@ typedef struct{
 #define EXTI					 			((EXTI_RegDef_t*)EXTI_BASEADDR)
 #define SYSCFG					 			((SYSCFG_RegDef_t*)SYSCFG_BASEADDR)
 
+#define SPI1								((SPI_RegDef_t*)SPI1_BASEADDR)
+#define SPI2								((SPI_RegDef_t*)SPI2_BASEADDR)
+#define SPI3								((SPI_RegDef_t*)SPI3_BASEADDR)
+#define SPI4								((SPI_RegDef_t*)SPI4_BASEADDR)
+
 // Clock enable Macros for GPIOx peripherals
 #define GPIOA_PCLK_EN()						(RCC->AHB1ENR |= ( 1 << 0))
 #define GPIOB_PCLK_EN()						(RCC->AHB1ENR |= ( 1 << 1))
@@ -202,6 +223,7 @@ typedef struct{
 #define SPI1_PCLK_EN()						(RCC->APB2ENR |= ( 1 << 12))
 #define SPI2_PCLK_EN()						(RCC->APB1ENR |= ( 1 << 14))
 #define SPI3_PCLK_EN()						(RCC->APB1ENR |= ( 1 << 15))
+#define SPI4_PCLK_EN()						(RCC->APB1ENR |= ( 1 << 13))
 
 // Clock enable Macros for USARTx peripherals
 #define USART1_PCLK_EN()					(RCC->APB2ENR |= ( 1 << 4))
@@ -235,6 +257,7 @@ typedef struct{
 #define SPI1_PCLK_DI()						(RCC->APB2ENR &= ~( 1 << 12))
 #define SPI2_PCLK_DI()						(RCC->APB1ENR &= ~( 1 << 14))
 #define SPI3_PCLK_DI()						(RCC->APB1ENR &= ~( 1 << 15))
+#define SPI4_PCLK_DI()						(RCC->APB2ENR &= ~( 1 << 13))
 
 // Clock disable Macros for USARTx peripherals
 #define USART1_PCLK_DI()					(RCC->APB2ENR &= ~( 1 << 4))
@@ -312,6 +335,49 @@ typedef struct{
 #define GPIO_PIN_SET						SET
 #define GPIO_PIN_RESET						RESET
 
+/*
+ *	Bit position definitions of SPI peripheral
+ */
+
+// Bit position definitions SPI_CR2
+#define SPI_CR1_CPHA						0
+#define SPI_CR1_CPOL						1
+#define SPI_CR1_MSTR						2
+#define SPI_CR1_BR							3
+#define SPI_CR1_SPE							6
+#define SPI_CR1_LSBFIRST					7
+#define SPI_CR1_SSI							8
+#define SPI_CR1_SSM							9
+#define SPI_CR1_RXONLY						10
+#define SPI_CR1_DFF							11
+#define SPI_CR1_CRCNEXT						12
+#define SPI_CR1_CRCEN						13
+#define SPI_CR1_BIDIOE						14
+#define SPI_CR1_BIDIMODE					15
+
+// Bit position definitions SPI_CR2
+#define SPI_CR2_RXDMAEN						0
+#define SPI_CR2_TXDMAEN						1
+#define SPI_CR2_SSOE						2
+#define SPI_CR2_FRF							4
+#define SPI_CR2_ERRIE						5
+#define SPI_CR2_RXNEIE						6
+#define SPI_CR2_TXEIE						7
+
+// Bit position definitions SPI_SR
+#define SPI_SR_RXNE							0
+#define SPI_SR_TXE							1
+#define SPI_SR_CHSIDE						2
+#define SPI_SR_UDR							3
+#define SPI_SR_CRCERR						4
+#define SPI_SR_MODF							5
+#define SPI_SR_OVR							6
+#define SPI_SR_BSY							7
+#define SPI_SR_FRE							8
+
+
+
 #include "stm32f407xx_gpio_driver.h"
+#include "stm32f407xx_spi_driver.h"
 
 #endif /* INC_STM32F407XX_H_ */
